492f7c8440705d594911bd03b9d823b2009341ab
zhuanke8 <64455988+zhuanke8@users.noreply.github.com>
2021-06-12 23:16:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main(user, passwd, step):
     
     if step == '':
         print ("已设置为随机步数（24000-25000）")
-        step = str(random.randint(24000-25000))
+        step = str(random.randint(24000,25000))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
